a77a415246d315938606550277f5d2d7c494f244
dyun monterey <dcmonterey1023@gmail.com>
2023-04-07 16:24:17 +0800

    Update package structure. Use conf/ and web/ modules

---
+++ web/src/main/java/com/recipe/recipemanagementapp/RecipeManagementApp.java
@@ -0,0 +1,13 @@
+package com.recipe.recipemanagementapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeManagementApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeManagementApp.class, args);
+	}
+
+}
